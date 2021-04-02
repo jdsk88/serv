8563fdef5f33fd4032c4ea8c38095369b25fb1dc
jdsk88 <maciejjakobszy@me.com>
2021-04-03 00:49:12 +0200

    prevent duplicating locations

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {data} from "../../data/places.js";
+import { Location } from "../models/locations.js";
 import {
   getLocation, getLocations,
   addLocation, updateLocation, destroyLocation,
@@ -36,13 +37,28 @@ routes.get("/:location_id", async (req, res) => {
 routes.post("/", async (req, res) => {
   const { name,type,address:{street,city,country,cords:{longitude,latitude}},contact:{first_name,last_name,email,phone},invoice_data:{nip,full_name,short_name,adress}
  } = req.body;
-
-
-
   const result = await addLocation({ name,type,address:{street,city,country,cords:{longitude,latitude}},contact:{first_name,last_name,email,phone},invoice_data:{nip,full_name,short_name,adress}
  });
   res.header("Access-Control-Allow-Origin", "*");
   res.send(result)
+});
+
+routes.post('/add', (req, res, next) => {
+  let query = req.body.name;
+  Location.findOne({name:query}, function(err, location){
+      if(err) console.log(err);
+      if ( location){
+          console.log("This location has already been saved");
+      } else {
+
+          var location = new Location(req.body);
+          location.save(function(err, location) {
+              if(err) console.log(err);
+              console.log("New location profile created");
+              res.redirect(`/api/locations`);
+          });
+      }
+  });
 });
 
 
